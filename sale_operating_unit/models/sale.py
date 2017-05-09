cba249677253a1b293d546536205d4d71cac2ca1
Liyong Wang <liyong.wang@outlook.com>
2017-05-09 14:36:30 +0800

    backports to v8.0

---
+++ sale_operating_unit/models/sale.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+# © 2015 Eficent Business and IT Consulting Services S.L.
+# - Jordi Ballester Alomar
+# © 2015 Serpent Consulting Services Pvt. Ltd. - Sudhir Arya
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+from openerp import _, api, fields, models
+from openerp.exceptions import ValidationError
+
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+    @api.model
+    def _default_operating_unit(self):
+        if self._defaults['team_id'](self):
+            return self.env['crm.case.section'].browse(self._defaults['team_id'](
+                self)).operating_unit_id
+        else:
+            return self.env.user.default_operating_unit_id
+
+    operating_unit_id = fields.Many2one(
+        comodel_name='operating.unit',
+        string='Operating Unit',
+        default=_default_operating_unit
+    )
+
+    @api.multi
+    @api.constrains('operating_unit_id', 'company_id')
+    def _check_company_operating_unit(self):
+        for rec in self:
+            if (rec.company_id and rec.operating_unit_id and
+                    rec.company_id != rec.operating_unit_id.company_id):
+                raise ValidationError(_('Configuration error\nThe Company in'
+                                        ' the Sales Order and in the Operating'
+                                        ' Unit must be the same.'))
+
+    @api.multi
+    def _prepare_invoice(self):
+        self.ensure_one()
+        invoice_vals = super(SaleOrder, self)._prepare_invoice()
+        invoice_vals['operating_unit_id'] = self.operating_unit_id.id
+        return invoice_vals
+
+
+class SaleOrderLine(models.Model):
+    _inherit = 'sale.order.line'
+
+    operating_unit_id = fields.Many2one(related='order_id.operating_unit_id',
+                                        string='Operating Unit',
+                                        readonly=True)
